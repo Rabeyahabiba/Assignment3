0bcc49874e66a7ae8c5742378a6937ff8c1118fe
Rabeyahabiba <76880601+Rabeyahabiba@users.noreply.github.com>
2021-01-22 18:36:59 +0600

    Delete new1.js

--- new1.js
+++
@@ -1,8 +0,0 @@
-function budgetCalculator(watch,phone,laptop){
-    
-    var result = (watch*50 )+ (phone*100) +(laptop*500) ;
-    return result;
-
-}
-var sum=budgetCalculator(2,4,3);
-console.log (sum);
